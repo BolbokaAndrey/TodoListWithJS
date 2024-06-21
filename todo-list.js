e1cf56fe26b36f75f50d63e355af38f947337834
Андрей <bolbocka@yandex.ru>
2024-06-22 02:20:58 +0300

    Done top

--- todo-list.js
+++ todo-list.js
@@ -1,2 +1,19 @@
-let taskDiv = document.createElement("div");
-taskDiv.classList.add("todo__task", "task");
+
+document.addEventListener('click', (event) => {
+    let searchInput = document.getElementById('search-input');
+    let addTaskInput = document.getElementById('add-task-input');
+
+    if (event.target.classList.contains('todo__search-btn')) {
+        if (searchInput.classList.contains('active')) {
+            console.log(searchInput.value);
+        } else {
+            searchInput.classList.toggle('active');
+            addTaskInput.classList.toggle('active');
+        }
+
+    } else if (!event.target.classList.contains('todo__search-input')) {
+        searchInput.classList.remove('active');
+        addTaskInput.classList.add('active');
+    }
+})
+
